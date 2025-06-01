474d118cd46a1d5e957cf2f818da0023886ce350
Amit K Sharma <shamamit11@users.noreply.github.com>
2025-06-01 11:20:38 +0200

    update: start.sh with pm2 full path

--- start.sh
+++ start.sh
@@ -1,7 +1,12 @@
-pm2 stop rsp-website
-pm2 delete rsp-website
+#!/bin/bash
+
+# Use full path to pm2
+PM2="/home/master/bin/npm/lib/node_modules/bin/pm2"
+
+$PM2 stop rsp-website
+$PM2 delete rsp-website
 
 npm install
 npm run build
 
-pm2 start npm --name "rsp-website" -- start
+$PM2 start npm --name "rsp-website" -- start
